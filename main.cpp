df506ae9a17cb109d9f986362221211245aa0a5a
i27ae15 <andresruse18@gmail.com>
2022-05-26 12:09:47 -0400

    while loop

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+
+// for loops
+// int main() {
+
+
+//     // for (int i = 0; i <= 10; i++) {
+
+//     //     cout << "there is your number: " << i << endl;
+        
+//     // }
+
+//     int list[] = {1, 2, 3, 4, 5, 6};
+//     int integer_value = 4;
+
+//     for (int i = 0; i < sizeof(list) / integer_value; i++){
+//         cout << "Your number: " << list[i] << endl; 
+
+//         if (list[i] == 4) {
+//             break;
+//         }
+//     }
+
+// }
+
+// while loops
+int main() {
+
+    char answer;
+
+    while (answer != 'y'){
+        cout << "wanna finish?" << endl;
+        cin >> answer;
+    }
+    
+    cout << "get the hell out";
+
+}
