b9927a8362e7a0741e07d98c1519b079bb38f28b
i27ae15 <andresruse18@gmail.com>
2022-07-01 16:45:08 -0400

    end class

---
+++ classes/main.cpp
@@ -0,0 +1,43 @@
+# include <iostream>
+
+using namespace std;
+
+// int main() {
+
+//     // char
+//     char middle_letter {'j'};
+
+
+//     // int
+//     // unsigned integers can just store positive integers
+//     unsigned short int exam_score {55};
+
+//     // long is for long integers
+//     long people_in_florida {2500000};
+
+//     // there is an advantage of initializing the numbers in a list type, it does return an error when stackoverflow
+//     long long people_on_earth {7'500'000'000};
+
+//     // float numbers 
+
+//     float payment {115.29};
+
+//     // a double is bigger than a float
+//     double pi {3.1415926535};
+
+//     long double = bigger_than_double_and_float {3.14e120};
+
+//     // boolean values 
+
+//     bool game_over {false};
+
+//     cout << payment << endl;
+
+// }
+    
+
+#include <climits>
+
+int main(){
+
+}
